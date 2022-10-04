a383315b5176c7db6c93a5c654397bfeaf92e278
Wendel <sr.wendelfernandes@outlook.com>
2022-10-04 16:32:32 -0300

    minor changes in component ImageCard and content in HomePage

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="container">
-  <div class="row mt-2 mb-4 image-card">
-    <div class="col-lg-6 col-sm-12 col-sm-3 p-4 pt-5 ps-5 ms-4">
+  <div class="container image-card"
+  :class="small && 'col-lg-2 image-card-small'">
+  <div class="row mt-2 mb-4 "
+  :class="reverse && 'image-card-reverse'">
+    <div class="col-lg-6 col-sm-12 col-sm-3 p-4 pt-5 ps-5 ms-4"
+    :class="vertical && 'col-lg-12 pt-0'">
       {{ text }}
     </div>
-    <div class="col-lg-5 col-sm-12 col-sm-9">
+    <div class="col-lg-5 col-sm-12 col-sm-9"
+    :class="vertical && 'col-lg-12 text-center'">
       <b-img :src="source" fluid alt="Responsive image"></b-img>
     </div>
   </div>
@@ -15,11 +19,20 @@
 
 .image-card {
   background: linear-gradient(135deg, #3A2369 0%, #78619B 100%);
-border-radius: 25px;
+  border-radius: 25px;
   font-size: 4rem;
   font-family: 'Jost';
   font-weight: bolder;
   color: white;
+  flex-direction: row;
+}
+
+.image-card-reverse {
+  flex-direction: column-reverse;
+}
+
+.image-card-small {
+  font-size: medium !important;
 }
 
 </style>
@@ -34,6 +47,18 @@ border-radius: 25px;
     source: {
       type: String,
       required: true
+    },
+    reverse: {
+      type: Boolean,
+      default: false
+    },
+    vertical: {
+      type: Boolean,
+      default: false
+    },
+    small: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,6 +2,7 @@
 import { RouterView } from "vue-router";
 import NavBar from "@/components/NavBar.vue";
 import FooterItem from "@/components/FooterItem.vue";
+import ImageCard from "../components/ImageCard.vue";
 </script>
 
 <template>
@@ -17,6 +18,14 @@ import FooterItem from "@/components/FooterItem.vue";
       },
     ]"
   />
+  <ImageCard text="Evite problemas ao criar sua Startup" 
+  source="https://hub.pme-digital.pt/wp-content/uploads/2019/04/Flat-color-Modern-Isometric-Illustration-design-Startup.png"
+/>
+<ImageCard text="Agilidade de Criação"
+source="https://hub.pme-digital.pt/wp-content/uploads/2019/04/Flat-color-Modern-Isometric-Illustration-design-Startup.png"
+small
+vertical
+reverse/>
   <FooterItem/>
   <div class="content">
     <RouterView />
