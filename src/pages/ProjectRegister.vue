933df1389bcb0b68cd56a4741e8ceefce4bf2410
Gigademolidor <gabriel_braga_lagrotaria_de_oliveira@usp.br>
2022-10-05 09:01:34 -0300

    progress-register(incomplete)

--- src/pages/ProjectRegister.vue
+++ src/pages/ProjectRegister.vue
@@ -1,7 +1,51 @@
 <script setup lang="ts">
 import ProgressIndicator from "../components/ProgressIndicator.vue";
-
 </script>
 <template>
-  <ProgressIndicator />
+  <div class="register-container container">
+    <div class="row d-flex align-items-center justify-content-center">
+      <div class="col-md-10 col-lg-8 col-sm-12">
+        <ProgressIndicator />
+      </div>
+    </div>
+    <div class="row d-flex flex-column align-items-center mt-5">
+      <div class="col-md-10 col-lg-8 col-sm-12">
+        <h1 class="page-section-title">
+          Envie um convite para os integrantes do projeto:
+        </h1>
+      </div>
+    </div>
+    <div class="row d-flex flex-column align-items-center">
+      <div class="col-md-10 col-lg-8 col-sm-12 mt-5">
+        <div class="input-container">
+          <input class="input-register" type="text" />
+        </div>
+      </div>
+      <div class="col-md-2 col-lg-8 col-sm-12 mt-5">
+        <button></button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+@import "@/assets/styles/base.css";
+.register-container {
+  width: 65%;
+  min-width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.input-container {
+  height: 2rem;
+  width: 60%;
+  background-color: white;
+}
+.input-register {
+  height: 100%;
+  width: 100%;
+  border: none;
+}
+</style>
