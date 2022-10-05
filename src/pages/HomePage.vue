9876021f0f06946fbd05fb3feaa0d969ab39d33d
Wendel <sr.wendelfernandes@outlook.com>
2022-10-04 21:25:58 -0300

    add elements in home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,15 +6,50 @@ import ImageCard from "../components/ImageCard.vue";
 </script>
 
 <template>
-  <NavBar :currentPage.number="1"/>
-  <ImageCard text="Evite problemas ao criar sua Startup"
+  <NavBar :currentPage.number="1"
+  />
+  <ImageCard title="Evite problemas ao criar sua Startup" 
   source="https://hub.pme-digital.pt/wp-content/uploads/2019/04/Flat-color-Modern-Isometric-Illustration-design-Startup.png"
 />
-<ImageCard text="Agilidade de Criação"
+<div class="container">
+  <div class="row" style="font-family: 'Jost'; font-size: 2rem;">
+    <article class="col-lg-2">Diferenciais do nosso projeto</article>
+<ImageCard title="Agilidade de Criação"
 source="https://hub.pme-digital.pt/wp-content/uploads/2019/04/Flat-color-Modern-Isometric-Illustration-design-Startup.png"
 small
 vertical
 reverse/>
+
+<ImageCard title="Aprenda com exemplos"
+source="https://hub.pme-digital.pt/wp-content/uploads/2019/04/Flat-color-Modern-Isometric-Illustration-design-Startup.png"
+small
+vertical
+reverse/>
+
+<ImageCard title="Inicie sua ideia online"
+source="https://hub.pme-digital.pt/wp-content/uploads/2019/04/Flat-color-Modern-Isometric-Illustration-design-Startup.png"
+small
+vertical
+reverse/>
+
+</div>
+</div>
+
+<ImageCard title="Publique e participe de ideias inovadoras"
+body="Com o Stewart você poderá participar de projetos inovadores, publicar suas ideias, e reunir pessoas interessadas no seu projeto para formar sua equipe."
+source="https://hub.pme-digital.pt/wp-content/uploads/2019/04/Flat-color-Modern-Isometric-Illustration-design-Startup.png"
+transparent
+titleSize="2.5rem"
+/>
+
+<ImageCard title="Ambiente seguro para suas ideias"
+body="Conte com o Stewart para proteger seus projetos, divulgando seus dados apenas para as pessoas que você quiser."
+source="https://hub.pme-digital.pt/wp-content/uploads/2019/04/Flat-color-Modern-Isometric-Illustration-design-Startup.png"
+reverse
+transparent
+titleSize="2.5rem"
+/>
+
   <FooterItem/>
   <div class="content">
     <RouterView />
