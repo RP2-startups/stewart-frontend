74679efd071bf1c101da0020696c461a6976a69c
felmateos <felmateos@usp.br>
2022-11-15 19:56:06 -0300

    Added person card and propV to Project.vue

---
+++ src/components/PersonCard.vue
@@ -0,0 +1,72 @@
+<template name="" funtion="">
+    <div class="person-card-container">
+        <img src="https://avatars.githubusercontent.com/u/88856792?v=4" alt="Avatar" class="person-card-image">
+        <div class="person-card-overlay">
+            <div class="person-card-name">{{ name }}</div>
+            <div class="person-card-function">{{ func }}</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.person-card-container {
+    position: relative;
+    width: 20vw;
+}
+
+.person-card-image {
+    display: block;
+    width: 20vw;
+    height: auto;
+}
+
+.person-card-overlay {
+    position: absolute;
+    top: 70%;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 30%;
+    width: 100%;
+    opacity: 0;
+    transition: .5s ease;
+    background-color: #008CBA;
+}
+
+.person-card-container:hover .person-card-overlay {
+    opacity: 1;
+}
+
+.person-card-name {
+    color: white;
+    font-size: 20px;
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+.person-card-function {
+    color: white;
+    font-size: 20px;
+    position: absolute;
+    top: 60%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+</style>
+
+<script lang="ts">
+export default {
+    props: [
+        'name',
+        'func'
+    ]
+}
+</script>
--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
 import ProjectHeader from "@/components/ProjectHeader.vue";
+import Members from "@/subpages/Members.vue";
 </script>
 
 <template>
-    <ProjectHeader />
+    <!--topo da pagina do porojeto (contem o banner, icone, estatisticas e descricao)-->
+    <ProjectHeader :propV="[
+        'veio risonho', 'texto ae ']
+        "/>
+
+    <!--conteúdo da subpagina "integrantes" (contem os cards de todos os integrantes do projeto)-->
+    <div class="proj-page-content">
+        <Members />
+    </div>
 </template>
+
+<style scoped>
+    .proj-page-content {
+        margin-inline: min(10vw, 100px);
+    }
+</style>
