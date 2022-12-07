bd408355f8cf6120a58d985ec8d2fe5af1d0958b
mpeco <matpecor@gmail.com>
2022-12-06 23:21:12 -0300

    carousel component implemented

--- src/components/ImageCardCarousel.vue
+++ src/components/ImageCardCarousel.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   import ImageCard from "../components/ImageCard.vue";
+  import 'vue3-carousel/dist/carousel.css'
+  import { Carousel, Slide, Navigation } from 'vue3-carousel'
 </script>
 
 <template>
@@ -7,16 +9,17 @@
     <div class="d-flex section-title">
       <p class="flex-fill"> {{ title }} </p>
     </div>
-    <div class="section-hovered" id="box">
-      <div v-if="cards.length > 0" class="section-items row" :class="rowColsClass">
-        <div class="col" v-for="card in cards">
-          <ImageCard class="pt-1" :width="cardWidth" :height="cardHeight"
-            :title="card.name" :body="card.desc" :source="card.icon" image-border />
-        </div>
-      </div>
-      <div v-else class="query-not-found">
-        {{ notFoundText }}
-      </div>
+    <div class="d-flex section-hovered" id="box">
+      <carousel style="width: 100%" :items-to-show="cardsPerRow+0.6" wrap-around snapAlign="start">
+        <slide v-for="(card, index) in cards" :key="index" slideWidth="100px">
+          <div class="section-items row">
+            <div class="col">
+              <ImageCard class="pt-1" :width="cardWidth" :height="cardHeight"
+                :title="card.name" :body="card.desc" :source="card.icon" image-border />
+            </div>
+          </div>
+        </slide>
+      </carousel>
     </div>
   </div>
 </template>
@@ -43,10 +46,7 @@
   }
 
   .section-items {
-    padding-top: 1rem;
-    padding-left: 3rem;
-    padding-right: 3rem;
-    height: 800px;
+    max-height: 450px;
   }
   #box {
     background-color: var(--color-translucent-background-dark);
@@ -54,13 +54,8 @@
     border-top-width: 0px;
     border-style: solid;
     border-color: var(--color-purple-dark);
-    overflow-y: auto;
+    overflow-y: clip;
     overflow-x: hidden;
-  }
-  .query-not-found{
-    padding: 3rem;
-    font-size: 3rem;
-    text-align: center;
   }
 </style>
 
@@ -93,12 +88,11 @@
       return {
         isCollapsed: false,
         cardsPerRow: 1,
-        cards: [{ name: "Among Us", desc: "Among Us é um jogo", icon: "https://preview.redd.it/otcm08c8oyy51.jpg?auto=webp&s=7924c636151996440520b3a9c94e70f59792491e" }],
-        rowColsClass: 'row-cols-1',
+        cards: [{name: "", desc: "", icon: ""}],
       };
     },
     created() {
-        //this.cards = this.cardsProp as any;
+        this.cards = this.cardsProp as any;
     },
     mounted() {
       this.calcCardsPerRow();
@@ -108,9 +102,8 @@
       calcCardsPerRow() {
         let remSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
         let sectionEl = this.$refs.section as Element;
-        let availableWidth = sectionEl.clientWidth - 6*remSize;
+        let availableWidth = sectionEl.clientWidth - 16*remSize;
         this.cardsPerRow = Math.floor(availableWidth/this.cardWidth);
-        this.rowColsClass = (this.cardsPerRow <= 6) ? "row-cols-" + this.cardsPerRow : "row-cols-6"
       },
     },
   });
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,14 +3,32 @@
 </script>
 
 <template>
-  <ImageCardCarousel :cardWidth="240" :cardHeight="400" title="Projetos Recomendados:" notFoundText="Nenhum projeto encontrado." />
+  <div class="carousel">
+    <ImageCardCarousel :cardsProp="cards" :cardWidth="300" :cardHeight="400" title="Projetos Recomendados:" notFoundText="Nenhum projeto encontrado." />
+  </div>
 </template>
 
 <style>
 @import "../assets/styles/base.css";
 
-.text {
-  font-family: 'Jost';
-  font-size: 2rem;
+.carousel {
+  margin: 2rem;
 }
 </style>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    data() {
+      return {
+        cards: [
+          { name: "1", desc: "Among Us é um jogo", icon: "https://preview.redd.it/otcm08c8oyy51.jpg?auto=webp&s=7924c636151996440520b3a9c94e70f59792491e" },
+          { name: "2", desc: "Among Us é um jogo", icon: "https://preview.redd.it/otcm08c8oyy51.jpg?auto=webp&s=7924c636151996440520b3a9c94e70f59792491e" },
+          { name: "3", desc: "Among Us é um jogo", icon: "https://preview.redd.it/otcm08c8oyy51.jpg?auto=webp&s=7924c636151996440520b3a9c94e70f59792491e" },
+          { name: "4", desc: "Among Us é um jogo", icon: "https://preview.redd.it/otcm08c8oyy51.jpg?auto=webp&s=7924c636151996440520b3a9c94e70f59792491e" },
+          { name: "5", desc: "Among Us é um jogo", icon: "https://preview.redd.it/otcm08c8oyy51.jpg?auto=webp&s=7924c636151996440520b3a9c94e70f59792491e" },
+        ],
+      };
+    },
+  });
+</script>
